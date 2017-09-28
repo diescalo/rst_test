c99f223f2e5c87a90925d02c1b561dd1080df82b
Diego Escalona <diego.escalona@digi.com>
2017-09-28 15:01:35 +0200

    theme: add file theme_overrides.css to override the tables behavior
    Signed-off-by: Diego Escalona <diego.escalona@digi.com>

--- doc/conf.py
+++ doc/conf.py
@@ -96,6 +96,9 @@ html_theme = 'default'
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
+def setup(app):
+   app.add_stylesheet("theme_overrides.css")
+
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
 #
